e66aac0aa2a28fdb472370d145a4a969a78816aa
Jacomar13 <jacomar03@gmail.com>
2023-04-19 10:18:24 +0200

    Ejercicio 3 del Bloque 6: Path variables y headers 2/4

--- block6-path-variable-headers/src/main/java/javier/correa/block6pathvariableheaders/controllers/GreetingController.java
+++ block6-path-variable-headers/src/main/java/javier/correa/block6pathvariableheaders/controllers/GreetingController.java
@@ -10,14 +10,15 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
-    /*@PostMapping("/sendbody")
-    public Person sendBody(@RequestBody ){
-
-    }*/
+    @PostMapping("/sendbody")
+    public Person sendBody(@RequestBody Person personPassed){
+        return personPassed;
+    }
 }
