bf4dc1eedf3e5dce8303fa25ecf74fb252ecd52c
Jacomar13 <jacomar03@gmail.com>
2023-04-19 10:48:28 +0200

    Ejercicio 3 del Bloque 6: Path variables y headers 3/4

--- block6-path-variable-headers/src/main/java/javier/correa/block6pathvariableheaders/controllers/GreetingController.java
+++ block6-path-variable-headers/src/main/java/javier/correa/block6pathvariableheaders/controllers/GreetingController.java
@@ -4,13 +4,15 @@ import javier.correa.block6pathvariableheaders.objectsAndRecords.Greeting;
 import javier.correa.block6pathvariableheaders.objectsAndRecords.Person;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    
+
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
@@ -18,7 +20,22 @@ public class GreetingController {
     }
 
     @PostMapping("/sendbody")
-    public Person sendBody(@RequestBody Person personPassed){
+    public Person postSendBody(@RequestBody Person personPassed){
         return personPassed;
     }
+
+    @GetMapping("/user/{id}")
+    public int getGetAndPassUserId(@PathVariable int id){
+        return id;
+    }
+
+    /*@PutMapping("/post")
+    public Map<String, Integer> putPassVariables(@RequestParam Map<String, String> paramsRequested){
+        Map <String, Integer> nombreMap = new HashMap<String, Integer>();
+        for (Map.Entry<String,String> entry : paramsRequested.entrySet()) {
+            nombreMap.put(entry.getKey(), Integer.valueOf(entry.getValue()));
+        }
+        return nombreMap;
+    }*/
+
 }
--- block6-path-variable-headers/src/main/java/javier/correa/block6pathvariableheaders/objectsAndRecords/Person.java
+++ block6-path-variable-headers/src/main/java/javier/correa/block6pathvariableheaders/objectsAndRecords/Person.java
@@ -1,5 +1,4 @@
 package javier.correa.block6pathvariableheaders.objectsAndRecords;
 
 public record Person(long id, String name) {
-
 }
