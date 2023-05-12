8f2fc93990125ae77f6093a7dd44e1d8988c5ad7
Jacomar13 <jacomar03@gmail.com>
2023-05-12 09:25:26 +0200

    Ejercicio 2 del Bloque 7: CRUD con validación 4/4, subida de POSTMAN

--- block7-crud-validation/src/main/java/javier/correa/block7crudvalidation/application/ProfesorClient.java
+++ block7-crud-validation/src/main/java/javier/correa/block7crudvalidation/application/ProfesorClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(url = "http://localhost:8081", name = "profesorCliente")
+@FeignClient(url = "http://localhost:8081", name = "profesorClient")
 public interface ProfesorClient {
     @GetMapping("/profesor/{id}")
     public ProfesorOutputDto getProfesor(@PathVariable int id);
