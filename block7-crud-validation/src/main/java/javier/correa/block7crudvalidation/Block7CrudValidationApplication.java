193c887610d3c99ac0199d2aebbf5d1bbecda4ee
Jacomar13 <jacomar03@gmail.com>
2023-05-30 13:10:36 +0200

    Ejercicio 2 del Bloque 7: CRUD con validación

--- block7-crud-validation/src/main/java/javier/correa/block7crudvalidation/Block7CrudValidationApplication.java
+++ block7-crud-validation/src/main/java/javier/correa/block7crudvalidation/Block7CrudValidationApplication.java
@@ -3,8 +3,7 @@ package javier.correa.block7crudvalidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 @EnableFeignClients
 @SpringBootApplication
@@ -13,9 +12,5 @@ public class Block7CrudValidationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Block7CrudValidationApplication.class, args);
 	}
-	/*@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}*/
 
 }
