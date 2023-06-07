1fdd9af06552faf6633c31c7095370284d20f3ce
Jacomar13 <jacomar03@gmail.com>
2023-06-07 10:02:26 +0200

    Ejercicio 1 del Bloque 13: Testing CRUD, Tests de StudentTopicImpl creados

---
+++ block13-testing/src/test/java/javier/correa/block7crudvalidation/application/StudentTopicServiceImplTest.java
@@ -0,0 +1,32 @@
+package javier.correa.block7crudvalidation.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTopicServiceImplTest {
+
+    @Test
+    void addTopic() {
+    }
+
+    @Test
+    void getAllTopics() {
+    }
+
+    @Test
+    void getListOfTopicByStudent() {
+    }
+
+    @Test
+    void deleteTopicById() {
+    }
+
+    @Test
+    void updateTopic() {
+    }
+
+    @Test
+    void getTopicById() {
+    }
+}
